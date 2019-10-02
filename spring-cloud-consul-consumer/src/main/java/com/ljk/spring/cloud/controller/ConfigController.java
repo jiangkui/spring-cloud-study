27532638202a11c5f8d3214d7fa01292c392275d
jiangkui <jiangkui@meituan.com>
2019-10-02 16:27:38 +0800

    新增 config refresh demo

--- spring-cloud-consul-consumer/src/main/java/com/ljk/spring/cloud/controller/ConfigController.java
+++ spring-cloud-consul-consumer/src/main/java/com/ljk/spring/cloud/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.ljk.spring.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jiangkui
  * @version 1.0, 2019-10-02 07:27
  */
+@RefreshScope
 @RestController
 public class ConfigController {
 
