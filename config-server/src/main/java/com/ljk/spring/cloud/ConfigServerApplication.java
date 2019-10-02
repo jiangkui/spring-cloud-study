9a432ff2a55c6ec9541da9a49bff711978f7e114
jiangkui <jiangkui@meituan.com>
2019-10-02 11:19:21 +0800

    微调

--- config-server/src/main/java/com/ljk/spring/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/ljk/spring/cloud/ConfigServerApplication.java
@@ -2,19 +2,19 @@ package com.ljk.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * spring config server Demo：
- * - http://www.ityouknow.com/springcloud/2017/05/22/springcloud-config-git.html
- * - https://spring.io/projects/spring-cloud-config
- * - git 账号密码加密：https://cloud.tencent.com/developer/article/1081843
+ * - git 账号密码加密：https://cloud.spring.io/spring-cloud-config/reference/html/#_encryption_and_decryption
  *
  * @author jiangkui
  * @version 1.0, 2019-10-02 06:51
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
