c3dc602dc61b3922b7c3fd4e7f1dee919ce6b681
jiangkui <jiangkui@meituan.com>
2019-10-03 11:23:42 +0800

    新增 spring netflix zuul 相关demo

---
+++ spring-cloud-zuul/src/main/java/com/ljk/spring/cloud/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.ljk.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author jiangkui
+ * @version 1.0, 2019-10-03 11:11
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
